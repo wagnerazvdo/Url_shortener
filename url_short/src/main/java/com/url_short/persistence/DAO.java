c46bca4df8d4001cdf543913286f966b898545c4
wagnerazvdo <wagner_azvdo@outlook.com>
2021-08-25 16:34:53 -0300

    primeiro commit

---
+++ url_short/src/main/java/com/url_short/persistence/DAO.java
@@ -0,0 +1,29 @@
+package com.url_short.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DAO {
+	
+	protected ResultSet resultSet;
+	protected Connection connection;
+	protected PreparedStatement statement;
+	
+	private static final String DATABASE = "url_shortener?serverTimezone=UTC";
+	private static final String USER	 = "root";
+	private static final String PASS 	 = "admin123";
+	private static final String DRIVER   = "com.mysql.cj.jdbc.Driver";
+	private static final String URL		 = "jdbc:mysql://localhost:3306/";
+
+	public void open()throws Exception {
+		 Class.forName(DRIVER);
+		 connection = DriverManager.getConnection(URL+DATABASE,USER,PASS);
+	}
+	
+	public void close()throws Exception{
+		if(!connection.isClosed())
+			connection.close();
+	}
+}
---
+++ url_short/src/main/java/com/url_short/persistence/LinkDAO.java
@@ -0,0 +1,87 @@
+package com.url_short.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.url_short.model.LinkBean;
+
+public class LinkDAO  extends DAO{
+
+	private static final String INSERT    = "insert into tb_link(url, code, insert_date) values(?,(select substr(to_base64((select count(1) as total from tb_link l)),1,7)), sysdate());\n"
+			+ "";
+	private static final String UPDATE    = "update tb_link set custom =? where id =?";
+	private static final String FIND_ALL  = "select id, url, code, custom from tb_link;";
+	public boolean create(String url)throws Exception{
+		try {
+			open();
+		    statement = connection.prepareStatement(INSERT);
+		    statement.setString(1 , url);
+		    return statement.execute();
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			return false;
+		}finally {
+			
+			close();
+		}
+		 
+	}
+	
+	public List<LinkBean> findAll()throws Exception{
+		List<LinkBean> links = null;
+		try {
+			open();
+		    statement = connection.prepareStatement(FIND_ALL );
+		    resultSet = statement.executeQuery();
+		    links = new ArrayList<LinkBean>();
+		    while(resultSet.next()) {
+		    	links.add(new LinkBean(resultSet.getInt("id"), resultSet.getString("url"),
+		    						 resultSet.getString("code"),resultSet.getString("custom")));
+		    	 
+		    }
+		    //return statement.execute();
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			return null;
+		}finally {
+			close();
+		}
+		return links;
+		 
+	}
+	
+	public boolean update(String custom, int id)throws Exception{
+		try {
+			open();
+		    statement = connection.prepareStatement(UPDATE);
+		    statement.setString(1 , custom );
+		    statement.setInt(2, id);
+		    return statement.executeUpdate()>0 ? true : false ;
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			return false;
+		}finally {
+			
+			close();
+		}
+		 
+	}
+	
+	public static void main(String[] args) {
+		try {
+			LinkDAO lDAO = new LinkDAO();
+			System.out.println(lDAO.update("WG", 1));
+			lDAO.create("https://www.google.com/flamengo");
+			lDAO.findAll(); 
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage()); 
+		}
+	}
+	
+} 
+
